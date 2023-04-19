c8ff74efcade47bdafa7fce799fe9e006b323ae3
Lais <lais.salesms@gmail.com>
2023-04-19 11:19:27 -0300

    Criação do Projeto com CRUD e métodos opcionais

---
+++ src/main/java/com/generation/lojagames/controller/ProdutoController.java
@@ -0,0 +1,95 @@
+package com.generation.lojagames.controller;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.lojagames.model.Produto;
+import com.generation.lojagames.repository.CategoriaRepository;
+import com.generation.lojagames.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+	// Conectando com as Repositorys
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	// Métodos CRUD básicos
+	@PostMapping
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAll(){
+		return ResponseEntity.ok(produtoRepository.findAll());
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+		return produtoRepository.findById(produto.getId())
+				.map(respostaProdutoId -> 
+						categoriaRepository.findById(produto.getCategoria().getId())
+							.map(respostaCategoriaId -> ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto)))
+							.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build()))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> optional = produtoRepository.findById(id);
+		
+		if (optional.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		else {
+			produtoRepository.deleteById(id);
+		}
+	}
+	
+	// Métodos Adicionais
+	@GetMapping("{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id){
+		return produtoRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/preco_menor/{preco}")
+	public ResponseEntity<List<Produto>> getByPrecoMenor(@PathVariable BigDecimal preco){
+		return ResponseEntity.ok(produtoRepository.findAllByPrecoLessThanEqual(preco));
+	}
+	
+	@GetMapping("/preco_maior/{preco}")
+	public ResponseEntity<List<Produto>> getByPrecoMaior(@PathVariable BigDecimal preco){
+		return ResponseEntity.ok(produtoRepository.findAllByPrecoGreaterThanEqual(preco));
+	}
+}
---
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,126 @@
+package com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	// Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 1, max = 255, message = "O nome do produto deve ter entre 1 e 255 caracteres!")
+	private String nome;
+	
+	@NotBlank
+	@Size(min = 1, max = 510, message = "A descricao do produto deve ter entre 1 e 510 caracteres!")
+	private String descricao;
+	
+	@PositiveOrZero
+	private BigDecimal preco;
+	
+	private LocalDate data_lancamento;
+	
+	@NotBlank
+	@Size(min = 1, max = 255, message = "A plataforma do produto deve ter entre 1 e 255 caracteres!")
+	private String plataforma;
+	
+	@PositiveOrZero
+	private int quantidade;
+	
+	private boolean disponivel;
+	
+	// Relacionamento
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	// Get e Set do produto
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public LocalDate getData_lancamento() {
+		return data_lancamento;
+	}
+
+	public void setData_lancamento(LocalDate data_lancamento) {
+		this.data_lancamento = data_lancamento;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	// Get e Set da categoria
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
